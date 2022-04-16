87e90595e879f260bb98e68998a40defe07c1212
Kevin Williams <kevywilly@gmail.com>
2022-04-16 18:29:01 -0400

    initial import

---
+++ flash.sh
@@ -0,0 +1,1 @@
+ cp cmake-build-debug/pico_spider.uf2 /Volumes/RPI-RP2/pico_spider.uf2
---
+++ src/model.h
@@ -0,0 +1,9 @@
+//
+// Created by Kevin Williams on 4/16/22.
+//
+
+#ifndef PICO_SPIDER_MODEL_H
+#define PICO_SPIDER_MODEL_H
+extern const unsigned char g_model[];
+extern const int g_model_len;
+#endif //PICO_SPIDER_MODEL_H
